6c3b92e7cfd432c55213853de7cbe687f76c89e9
unknown <miyashaheen1995@gmail.com>
2021-02-11 22:30:10 +0530

    Added:Function to input co-ordinates of points & calculate length

--- LineComputationProgram.java
+++ LineComputationProgram.java
@@ -1,9 +1,31 @@
 package LineComparisonProblem;
 
+import java.util.Scanner;
+import java.lang.Math;
+
 public class LineComputationProgram {
 	
 	public static void main(String args[]) {
 		System.out.println("Welcome to Line Comparison Computation Program");
+		Scanner sc=new Scanner(System.in);
+		double length=CalculateLength(sc);
+		System.out.println("Length of Line is "+length);
+	}
+	
+	static double CalculateLength(Scanner sc) {
+		System.out.println("Enter Point1 co-ordinates:-");
+		System.out.println("Enter x1: ");
+		double x1_coordinate=sc.nextInt();
+		System.out.println("Enter y1: ");
+		double y1_coordinate=sc.nextInt();
+		System.out.println("Enter Point2 co-ordinates:-");
+		System.out.println("Enter x2: ");
+		double x2_coordinate=sc.nextInt();
+		System.out.println("Enter y2: ");
+		double y2_coordinate=sc.nextInt();
+		return Math.sqrt((x2_coordinate-x1_coordinate)*(x2_coordinate-x1_coordinate)+(y2_coordinate-y1_coordinate)*(y2_coordinate-y1_coordinate)); 
+		
+		
 	}
 
 }
